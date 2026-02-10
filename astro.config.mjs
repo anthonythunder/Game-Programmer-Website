596fe81bbfb3b04c23e6c8f64322c895a66d3308
Tony12X <dsouzaanthony781@gmail.com>
2026-02-11 00:04:57 +0530

    added project samples

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'http://localhost:4321/',// 'https://astro-blog-template.netlify.app',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
